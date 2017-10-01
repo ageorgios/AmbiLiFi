a27458c1609f8f0d41bd33c0ab6627f5d8226bfc
Georgios Andritsos <ageorgios@gmail.com>
2017-10-01 22:55:57 +0300

    Initial Commit

---
+++ AmbiLiFi/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AmbiLiFi
+//
+//  Created by Georgios Andritsos on 01/10/2017.
+//  Copyright © 2017 AppDVision. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AmbiLiFi/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AmbiLiFi
+//
+//  Created by Georgios Andritsos on 01/10/2017.
+//  Copyright © 2017 AppDVision. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
